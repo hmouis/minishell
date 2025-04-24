33c414847900e0953a37e5587deec4af278e5d18
oait-h-m <oussamaaithmed1@gmail.com>
2025-04-24 14:41:53 +0100

    remove some line and add enums

--- tokenization/type_of_tokens.c
+++ tokenization/type_of_tokens.c
@@ -16,54 +16,9 @@ int ft_strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while((s1[i] == s2[i]) && s1[i] && s2[i])
+	while(s1[i] && s2[i] && (s1[i] == s2[i]))
 		i++;
 	return (s1[i] - s2[i]);
-}
-
-int	len(char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return 0;
-	while (s[i])
-		i++;
-	return i;
-}
-
-int	builtin_command(t_lst *list, char **bu_command)
-{
-	int	i;
-
-	i = 0;
-	if (!list->content)
-		return 0;
-	while (bu_command[i])
-	{
-		if (!ft_strcmp(list->content, bu_command[i]))
-			return 1;
-		i++;
-	}
-	return 0;
-}
-
-int	external_command(t_lst *list, char **external_command)
-{
-	int	i;
-
-	i = 0;
-	if (!list->content)
-		return 0;
-	while (external_command[i])
-	{
-		if (!ft_strcmp(list->content, external_command[i]))
-			return 1;
-		i++;
-	}
-	return 0;
-
 }
 
 int	is_redirection(t_lst *list, char **opr)
@@ -82,40 +37,6 @@ int	is_redirection(t_lst *list, char **opr)
 	return 0;
 }
 
-int	is_env_variable(t_lst *list)
-{
-	if (!list->content)
-		return 0;
-	return (list->content[0] == '$');
-}
-
-int	is_single_quote(t_lst *list)
-{
-	if (!list->content)
-		return 0;
-	char	*s;
-	int	length;
-
-	length = 0;
-	s = list->content;
-	length = len(s);
-	return (s[0] == '\'' && s[length - 1] == '\'');
-}
-
-int	is_double_quote(t_lst *list)
-{
-	if (!list->content)
-		return 0;
-	char	*s;
-	int	length;
-
-	length = 0;
-	s = list->content;
-	length = len(s);
-	return (s[0] == '"' && s[length - 1] == '"');
-
-}
-
 int	is_pipe(t_lst *list)
 {
 	if (!list->content)
@@ -127,23 +48,11 @@ int	is_pipe(t_lst *list)
 
 void	tokens_type(t_lst **list)
 {
-	char	*bu_command[] = {"cd", "echo", "pwd", "export", "unset","env","exit", NULL};
-	char	*ect_command[] = {"ls", "cat", "grep", NULL};
-	char	*opr[] = {"<", ">", ">>", NULL};
-	if (builtin_command(*list, bu_command))
-		(*list)->type = "builtin command";
-	else if (external_command(*list, ect_command))
-		(*list)->type = "command";
-	else if (is_redirection(*list, opr))
-		(*list)->type = "redirection";
-	else if (is_env_variable(*list))
-		(*list)->type = "environment variable";
-	else if (is_single_quote(*list))
-		(*list)->type = "single quote";
-	else if (is_double_quote(*list))
-		(*list)->type = "double quote";
+	char	*opr[] = {"<<", ">>", ">", "<", NULL};
+	if (is_redirection(*list, opr))
+		(*list)->type = op_redirection;
 	else if (is_pipe(*list))
-		(*list)->type = "pipe";
+		(*list)->type = op_pipe;
 	else
-		(*list)->type = "argument";
+		(*list)->type = op_word;
 }
