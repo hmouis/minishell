bbe74b222f62eaf6d7e883d472223a1dc5785156
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-07 18:46:41 +0100

    add header file

--- minishell.h
+++ minishell.h
@@ -33,7 +33,7 @@ typedef enum e_types
 typedef struct s_lst
 {
 	char			*content;
-	enum e_types	type;
+	enum e_types	*type;
 	struct s_lst	*next;
 }					t_lst;
 
@@ -41,10 +41,12 @@ typedef struct s_var
 {
 	int				i;
 	int				y;
-	char			*token;
+	char				*token;
 	int				length;
-	char			*oldpwd;
-	char			*pwd;
+	char				*oldpwd;
+	char				*pwd;
+	char				*key;
+	char				*data_key;
 }					t_var;
 
 typedef struct s_cmd
@@ -55,9 +57,9 @@ typedef struct s_cmd
 }					t_cmd;
 
 
-void	free_lst(t_lst **lst);
-int pipe_line(t_lst *lst);
-int simple_command(t_lst **lst);
+void					free_lst(t_lst **lst);
+int					pipe_line(t_lst *lst);
+int					simple_command(t_lst **lst);
 int					is_redirection(t_lst *lst);
 int					is_pipe(t_lst *lst);
 int					ft_strcmp(char *s1, char *s2);
@@ -69,21 +71,21 @@ int					valid_operator(char c1, char c2);
 int					white_space(char c);
 int					is_operator(char *str);
 int					charchr(char *str, int c);
-char				*ft_strlcpy(char *token, char *input, int len, int j);
-t_lst				*last_node(t_lst *lst);
-void				add_back(t_lst **lst, t_lst *node);
-void				*new_node(char *content);
-void				add_to_lst(t_lst **lst, char *content);
+char					*ft_strlcpy(char *token, char *input, int len, int j);
+t_lst					*last_node(t_lst *lst);
+void					add_back(t_lst **lst, t_lst *node);
+void					*new_node(char *content);
+void					add_to_lst(t_lst **lst, char *content);
 int					split_input(char *input, t_lst **lst);
 int					token_quote(int *i, char *input, t_lst **lst, t_var *var);
 int					token_dollar_sign(int *i, char *input, t_lst **lst,
 						  t_var *var);
-void				tokens_type(t_lst *lst);
+void					tokens_type(t_lst *lst);
 
-int	is_builtins(char *cmd);
-char	*ft_strdup(char *s);
-void	ft_putstr_fd(char *s, int fd);
-int	builtin_cd(char *cwd, t_var *var);
+int					is_builtins(char *cmd);
+char					*ft_strdup(char *s);
+void					ft_putstr_fd(char *s, int fd);
+int					builtin_cd(char **cwd, t_var *var);
 
 
 #endif
