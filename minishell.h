36aa2b6a688c3e8cb0efcdd9f72e8ce83df3ff17
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-21 15:25:34 +0100

    add header file

--- minishell.h
+++ minishell.h
@@ -93,33 +93,33 @@ int				tokenize_dollar_sign(t_exp **exp, char *str);
 /*syntax errors*/
 void				error_msg(char *str);
 char				*check_quote(char *str);
-int					is_quote(char c);
+int				is_quote(char c);
 char				*pipe_line(t_lst *lst);
 char				*simple_command(t_lst **lst);
 void				add_to_lst_c(t_lst **lst, t_lst *node);
 t_lst				*new_node_c(t_lst *node);
 
 /*helper functions*/
-int					ft_strcmp(char *s1, char *s2);
-int					valid_operator(char c1, char c2);
-int					white_space(char c);
-int					is_operator(char *str);
-int					charchr(char *str, int c);
+int				ft_strcmp(char *s1, char *s2);
+int				valid_operator(char c1, char c2);
+int				white_space(char c);
+int				is_operator(char *str);
+int				charchr(char *str, int c);
 char				*ft_strlcpy(char *token, char *input, int len, int j);
 
 /*tokenizing*/
-int					token_dollar_sign(int *i, char *input, t_lst **lst, t_var *var);
-int					split_input(char *input, t_lst **lst);
-int					token_quote(int *i, char *input, t_lst **lst, t_var *var);
-int					token_operator(t_var *var, char *input, t_lst **lst);
-int					end_of_input(t_var *var, char *input, t_lst **lst);
-int					creat_token(t_var *var, char *input, t_lst **lst);
-int					token_blank(t_var *var, char *input, t_lst **lst);
+int				token_dollar_sign(int *i, char *input, t_lst **lst, t_var *var);
+int				split_input(char *input, t_lst **lst);
+int				token_quote(int *i, char *input, t_lst **lst, t_var *var);
+int				token_operator(t_var *var, char *input, t_lst **lst);
+int				end_of_input(t_var *var, char *input, t_lst **lst);
+int				creat_token(t_var *var, char *input, t_lst **lst);
+int				token_blank(t_var *var, char *input, t_lst **lst);
 
 /*type of token*/
 void				tokens_type(t_lst *lst);
-int					is_redirection(t_lst *lst);
-int					is_pipe(t_lst *lst);
+int				is_redirection(t_lst *lst);
+int				is_pipe(t_lst *lst);
 
 /*linked list*/
 t_cmd				*creat_cmd_struct(t_cmd **cmd, t_lst *lst);
@@ -130,15 +130,16 @@ void				*new_node(char *content);
 void				add_to_lst(t_lst **lst, char *content);
 
 /*get env*/
-int					is_builtins(char *cmd);
-int					builtin_cd(char **cwd, t_var *var);
+int				is_builtins(char *cmd);
+int				builtin_cd(char **cwd, t_var *var);
 void				builtin_env(t_env *env);
 void				builtin_echo(char **args);
 void				builtin_export(t_env **env, char *s);
-int					is_in_set(char c, const char *set);
+int				is_in_set(char c, const char *set);
 t_env				*env_new_node(char *key, char *data);
 char				*get_env(char *str, t_env *env);
-char				*get_key_env(char *env);
+char				*get_env_key(char *s);
+char				*get_env_data(char *s);
 char				*get_data_env(char *env);
 void				add_env_to_list(t_env **lst, char **env); 
 
@@ -146,15 +147,15 @@ void				add_env_to_list(t_env **lst, char **env);
 
 char				*ft_strcat(char *s1, char *s2);
 char				*ft_strtok(char *str, char *delim);
-int					split_char(char c);
+int				split_char(char c);
 char				*split_var_arg(char *str);
 
 //libft
 char				*ft_strdup(char *s);
 char				*ft_strncpy(char *dest, const char *src, size_t n);
-int					is_alpha(char c);
-int					is_alnum(char c);
-int					ft_is_digits(char c);
+int				is_alpha(char c);
+int				is_alnum(char c);
+int				ft_is_digits(char c);
 char				*ft_strchr(char *s, char c);
 char				*ft_substr(char *s, int start, int len);
 void				ft_putstr_fd(char *s, int fd);
