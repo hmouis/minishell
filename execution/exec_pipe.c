90b923ac856ca2827839ddefc0596ca44e43d32d
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-25 16:51:33 +0100

    create file for exec pipe

--- execution/exec_pipe.c
+++ execution/exec_pipe.c
@@ -11,3 +11,32 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	existing_pipes(t_final_struct *list)	
+{
+	if (!list)
+		return -1;
+	if (list->next == NULL)
+		return 0;
+	return 1;
+}
+
+int	exec_pipe(t_final_struct *list)
+{
+	int	fd[2];
+	if (pipe(fd) == -1)
+	{
+		printf("error: failed pipe\n");
+		return -1;
+	}
+	int childpid = fork();
+	if (childpid == 0)
+	{
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[0]);
+	}
+	return 1;
+}
