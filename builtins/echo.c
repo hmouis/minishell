ab394ec50f9bd68fd030cafcdbd3286da61d1260
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-25 10:03:52 -1000

    add builtin echo

---
+++ builtins/echo.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 06:40:56 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/05/25 10:03:34 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	builtin_echo(char **args)
+{
+	int	newline;
+	int	i;
+
+	i = 1;
+	newline = 0;
+	if (args[i] && ft_strcmp(args[i], "-n") == 0)
+	{
+		newline = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+}
