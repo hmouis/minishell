892781d7d4c5d0f77fc5a660c5b38008f67f597a
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-31 15:27:53 +0100

    fix cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,13 +6,38 @@
 /*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:44:50 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/05/26 09:45:17 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/05/31 14:44:24 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	exec_cd()
+void	exec_cd(t_env **env, t_exec **cmd)
 {
-	return;
+	char	*new_dir;
+
+	if (!cmd || !(*cmd) || !env || !(*env))
+		return ;
+	(*env)->oldpwd = getcwd(NULL, 0);
+	if ((*cmd)->args[1] && (*cmd)->args[2])
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		(*cmd)->exit_status = 1;
+		return ;
+	}
+	if (!(*cmd)->args[1])
+		new_dir = getenv("HOME");
+	else
+		new_dir = (*cmd)->args[1];
+	if (chdir(new_dir) == -1)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(new_dir , 2);
+		perror(":");
+		(*cmd)->exit_status = 1;
+		return ;
+	}
+	(*env)->pwd = getcwd(NULL, 0);
+	update_env(env, (*env)->oldpwd, (*env)->pwd);
+	(*cmd)->exit_status = 0;
 }
