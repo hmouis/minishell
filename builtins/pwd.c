11ae6500a3696887e7642ea1763a814af2261f63
oait-h-m <oussamaaithmed1@gmail.com>
2025-05-31 15:29:41 +0100

    fix error

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec_pwd.c                                         :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:36:58 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/05/26 09:39:50 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/05/31 14:50:57 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	exec_pwd()
+void	exec_pwd(t_env **env)
 {
 	char	*buffer;
 
 	buffer = getcwd(NULL, 0);
 	if (!buffer)
 	{
-		perror("getcwd");
+		if ((*env)->pwd)
+			printf("%s\n", (*env)->pwd);
+		else
+			perror("pwd");
 	}
-	printf("%s\n", buffer);
+	else
+		printf("%s\n", buffer);
 }
