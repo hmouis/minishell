5f4ce911a1542330ccd780bf90abf0cc19ac2f2d
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-03 15:05:20 +0100

    add apply redirection

--- execution/apply_redirect.c
+++ execution/apply_redirect.c
@@ -36,10 +36,47 @@ int	append_(char* filename)
 	return fd;
 }
 
+void	handle_input(int *fd, char *file)
+{
+	*fd = open(file, O_RDONLY);
+	if (*fd < 0)
+	{
+		perror("minishell");
+		return ;
+	}
+	dup2(*fd, STDIN_FILENO);
+	close(*fd);
+}
+
+void	handle_output(int *fd, char *file)
+{
+	*fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (*fd < 0)
+	{
+		perror("");
+		return ;
+	}
+	dup2(*fd, STDOUT_FILENO);
+	close(*fd);
+}
+
+void	handle_append(int *fd, char *file)
+{
+	*fd = append_(file);
+	 if (*fd < 0)
+	{
+		perror("");
+		return ;
+	}
+	dup2(*fd, STDOUT_FILENO);
+	close(*fd);
+}
 int	apply_redirect(t_final_struct *struc)
 {
-	int	fd = -1;
+	int	fd;
+	int	status;
 	int	redirect;
+	char	*file;
 	t_final_struct	*tmp = struc;
 
 	while (tmp->redirect)
@@ -49,38 +86,14 @@ int	apply_redirect(t_final_struct *struc)
 			ft_putstr_fd("syntax error: missing file for redirection\n", 2);
 			return -1;
 		}
-
 		redirect = type_of_redirect(tmp->redirect->str);
+		file = tmp->redirect->next->str;
 		if (redirect == op_redirect_input && tmp->redirect->type != -1)
-		{
-			fd = open(tmp->redirect->next->str, O_RDONLY);
-			if (fd < 0)
-			{
-				perror("minishell");
-				return -1;
-			}
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-		}
+			handle_input(&fd, file);
 		else if (redirect == op_redirect_output && tmp->redirect->type != -1)
-		{
-			fd = open(tmp->redirect->next->str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd < 0)
-			{
-				perror("");
-				return -1;
-			}
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			handle_output(&fd, file);
 		else if (redirect == op_append && tmp->redirect->type != -1)
-		{
-			fd = append_(tmp->redirect->next->str);
-			if (fd < 0)
-				return -1;
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			handle_append(&fd, file);
 		else
 			return -1;
 		tmp->redirect = tmp->redirect->next->next;
