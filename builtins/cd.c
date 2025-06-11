11a5d0e78963333531876754bdf57ba4d1b69610
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-11 17:14:26 +0100

    add exit status

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,26 +6,22 @@
 /*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:44:50 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/05/31 14:44:24 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/06/10 14:23:55 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
-void	exec_cd(t_env **env, t_exec **cmd)
+int		exec_cd(t_env **env, t_exec **cmd)
 {
 	char	*new_dir;
-	int	status;
 	if (!cmd || !(*cmd) || !env || !(*env))
-		return ;
+		return 1;
 	(*env)->oldpwd = getcwd(NULL, 0);
 	if ((*cmd)->args[1] && (*cmd)->args[2])
 	{
 		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
-		status = 1;
-		(*cmd)->exit_status = &status;
-		return ;
+		return 1;
 	}
 	if (!(*cmd)->args[1])
 		new_dir = getenv("HOME");
@@ -36,11 +32,9 @@ void	exec_cd(t_env **env, t_exec **cmd)
 		ft_putstr_fd("minishell: cd: ", 2);
 		ft_putstr_fd(new_dir , 2);
 		perror(":");
-		(*cmd)->exit_status = &status;
-		return ;
+		return 1;
 	}
 	(*env)->pwd = getcwd(NULL, 0);
 	update_env(env, (*env)->oldpwd, (*env)->pwd);
-	status = 1;
-	(*cmd)->exit_status = &status;
+	return 0;
 }
