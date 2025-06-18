ffa0f0defacc543b9f79b13fea32b4fbe38fb0b8
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-18 10:01:42 +0100

    add execute

--- execution/exec_pipe.c
+++ execution/exec_pipe.c
@@ -6,16 +6,18 @@
 /*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 04:43:48 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/05/23 04:44:05 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/06/17 12:26:57 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	child_process(t_final_struct *fnl, int in_fd, int out_fd, t_env *lst_env, char **env, t_exec **cmd)
+void child_process(t_final_struct *fnl,int in_fd, int out_fd, t_env *lst_env, char **env, t_exec **exec)
 {
+	int	status;
+	if (!fnl || !lst_env || !exec)
+		exit(EXIT_FAILURE);
 	apply_redirect(fnl);
-
 	if (in_fd != STDIN_FILENO)
 	{
 		dup2(in_fd, STDIN_FILENO);
@@ -26,20 +28,32 @@ static void	child_process(t_final_struct *fnl, int in_fd, int out_fd, t_env *lst
 		dup2(out_fd, STDOUT_FILENO);
 		close(out_fd);
 	}
-
-	int status = exec_cmd(env, cmd, fnl);
-	exit(status); 
+	exec_cmd(env, exec, fnl);
 }
 
 void	execute(t_final_struct *list, t_env *lst_env, char **env)
 {
-	int		fd[2];
-	int		in_fd = STDIN_FILENO;
-	int		status = 0;
-	pid_t		pid;
-	t_exec *exec; 
+	int fd[2];
+	int in_fd = STDIN_FILENO;
+	int status;
+	pid_t pid;
+	t_exec *exec;
 	t_final_struct *cmd = list;
-	int i = 0;
+
+	if (cmd && !cmd->next && cmd->args && is_builtins(cmd->args->str) != -1)
+	{
+		int saved_in = dup(STDIN_FILENO);
+		int saved_out = dup(STDOUT_FILENO);
+		apply_redirect(cmd);
+		exec = gnl_to_array(cmd->args);
+		exec_builtins(&lst_env, &exec, cmd);
+		dup2(saved_in, STDIN_FILENO);
+		dup2(saved_out, STDOUT_FILENO);
+		close(saved_in);
+		close(saved_out);
+		return;
+	}
+
 	while (cmd)
 	{
 		exec = gnl_to_array(cmd->args);
@@ -50,18 +64,6 @@ void	execute(t_final_struct *list, t_env *lst_env, char **env)
 			fd[0] = STDIN_FILENO;
 			fd[1] = STDOUT_FILENO;
 		}
-		if (is_builtins(cmd->args->str) != -1)
-		{
-			exec_builtins(&lst_env, &exec, list);
-			if (in_fd != STDIN_FILENO)
-				close(in_fd);
-			if (cmd->next)
-				close(fd[1]);
-			in_fd = fd[0];
-			list = list->next;
-			cmd = cmd->next;
-			continue;
-		}
 		pid = fork();
 		if (pid == 0)
 			child_process(cmd, in_fd, fd[1], lst_env, env, &exec);
@@ -70,7 +72,6 @@ void	execute(t_final_struct *list, t_env *lst_env, char **env)
 		if (cmd->next)
 			close(fd[1]);
 		in_fd = fd[0];
-		list = list->next;
 		cmd = cmd->next;
 	}
 	while (wait(&status) > 0)
@@ -80,3 +81,4 @@ void	execute(t_final_struct *list, t_env *lst_env, char **env)
 	else
 		g_exit_status = 1;
 }
+
