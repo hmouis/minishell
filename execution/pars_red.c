e89d92b912ef147e775c3e557cc579f6092e522d
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-21 14:52:45 +0100

    add pars redirect

---
+++ execution/pars_red.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pars_red.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 14:42:29 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/06/21 14:42:47 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+
+int	pars_red(t_gnl *red)
+{
+	int	count;
+
+	count = 0;
+	while (red)
+	{
+		red = red->next;
+		while (red && (red->type == -1 || red->type == var))
+		{
+			count++;
+			if (count > 1)
+			{
+				ft_putstr_fd("minishell: ambiguous redirect\n", 2);
+				return (0);
+			}
+			red = red->next;
+		}
+		count = 0;
+	}
+	return (1);
+}
