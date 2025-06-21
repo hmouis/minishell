5fdc88816564ad2c5fcb7de4b0009eb049641406
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-21 14:50:29 +0100

    add or update env

---
+++ builtins/add_or_update_env.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_or_update_env.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 14:16:26 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/06/21 14:17:14 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	add_or_update_env(t_env **env, char *key, char *value)
+{
+	t_env	*cur;
+	t_env	*new_node;
+	t_env	*last;
+
+	last = NULL;
+	cur = *env;
+	while (cur)
+	{
+		if (ft_strcmp(cur->key, key) == 0)
+		{
+			cur->data = NULL;
+			cur->data = ft_strdup(value);
+			return ;
+		}
+		last = cur;
+		cur = cur->next;
+	}
+	new_node = env_new_node(ft_strdup(key), ft_strdup(value));
+	if (last)
+		last->next = new_node;
+	else
+		*env = new_node;
+}
