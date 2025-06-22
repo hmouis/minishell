6314dcfaeebee4aa8d08d6d8364ac845ddd05e63
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-22 14:34:58 +0100

    add builtins

---
+++ builtins/echo.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maelmahf <maelmahf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 06:40:56 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/06/21 15:58:20 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	print_with_exit_status(const char *arg)
+{
+	int	i;
+	int	in_single_quote;
+
+	i = 0;
+	in_single_quote = 0;
+	while (arg[i])
+	{
+		if (arg[i] == '\'')
+		{
+			in_single_quote = !in_single_quote;
+			putchar(arg[i]);
+			i++;
+		}
+		else if (!in_single_quote && arg[i] == '$' && arg[i + 1] == '?')
+		{
+			printf("%d", g_exit_status);
+			i += 2;
+		}
+		else
+		{
+			putchar(arg[i]);
+			i++;
+		}
+	}
+}
+
+void	exec_echo(t_exec **cmd)
+{
+	int	newline;
+	int	i;
+
+	newline = 1;
+	i = 1;
+	if ((*cmd)->args[i] && ft_strcmp((*cmd)->args[i], "-n") == 0)
+	{
+		newline = 0;
+		i++;
+	}
+	while ((*cmd)->args[i])
+	{
+		print_with_exit_status((*cmd)->args[i]);
+		if ((*cmd)->args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	g_exit_status = 0;
+}
