b0c840e1366df2cbae93e2125c7dce801adf7aa4
oait-h-m <oussamaaithmed1@gmail.com>
2025-06-30 02:42:33 +0100

    move some functions to another files for fix norminette

--- execution/exec_pipe.c
+++ execution/exec_pipe.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   exec_pipe.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmouis <hmouis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/24 22:25:37 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/06/29 14:06:56 by hmouis           ###   ########.fr       */
+/*   Created: 2025/06/30 02:05:17 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/06/30 02:05:18 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	child_process(t_final_struct *fnl, t_exec_pipe *var, t_env *lst_env,
-		char **env, t_exec **exec)
+static void	setup_redirections(t_final_struct *fnl, t_exec_pipe *var)
 {
 	int	input_redirected;
 	int	output_redirected;
@@ -32,94 +31,38 @@ void	child_process(t_final_struct *fnl, t_exec_pipe *var, t_env *lst_env,
 		dup2(var->fd[1], STDOUT_FILENO);
 		close(var->fd[1]);
 	}
+}
+
+void	child_process(t_child_params *params, t_exec **exec)
+{
+	setup_redirections(params->fnl, params->var);
 	if (is_builtins((*exec)->args[0]) != -1)
 	{
-		exec_builtins(&lst_env, exec, fnl);
+		exec_builtins(&params->lst_env, exec, params->fnl);
 		exit(EXIT_SUCCESS);
 	}
-	exec_cmd(env, exec, lst_env);
+	exec_cmd(params->env, exec, params->lst_env);
 	exit(EXIT_FAILURE);
-}
-
-static void	wait_for_children(int pid, int last_pid, int wstatus)
-{
-	int	track_sig;
-
-	track_sig = 0;
-	while ((pid = wait(&wstatus)) > 0)
-	{
-		if (WIFSIGNALED(wstatus))
-		{
-			if (WTERMSIG(wstatus) == SIGINT || WTERMSIG(wstatus) == SIGQUIT)
-				track_sig = 1;
-		}
-		if (pid == last_pid)
-		{
-			if (WIFEXITED(wstatus))
-				g_exit_status = WEXITSTATUS(wstatus);
-			else if (WIFSIGNALED(wstatus))
-				g_exit_status = 128 + WTERMSIG(wstatus);
-			else if (WIFSTOPPED(wstatus))
-				g_exit_status = 128 + WSTOPSIG(wstatus);
-		}
-	}
-	if (track_sig)
-		write(2, "\n", 1);
-}
-
-static int	exec_invalid_pipe_redirect(t_final_struct *list, t_env *lst_env,
-		t_exec_pipe **var)
-{
-	t_exec	*exec;
-
-	*var = ft_malloc(sizeof(t_exec_pipe), 1);
-	if (!*var)
-		exit(1);
-	if (list && !list->next && list->args && is_builtins(list->args->str) != -1
-		&& !list->redirect)
-	{
-		exec = gnl_to_array(list->args);
-		exec_builtins(&lst_env, &exec, list);
-		return (1);
-	}
-	return (0);
-}
-
-static void	initialize_fds(int *in_fd, int *out_fd)
-{
-	*in_fd = STDIN_FILENO;
-	*out_fd = STDOUT_FILENO;
-}
-
-static void	run_in_child(t_final_struct *list, t_exec_pipe *var, t_env *lst_env,
-		char **env, t_exec **exec)
-{
-	signal(SIGINT, SIG_DFL);
-	signal(SIGQUIT, SIG_DFL);
-	if (list->next)
-		close(var->fd[0]);
-	child_process(list, var, lst_env, env, exec);
 }
 
 void	execute(t_final_struct *list, t_env *lst_env, char **env)
 {
-	t_exec_pipe *var;
-	t_exec *exec;
+	t_exec_pipe		*var;
+	t_exec			*exec;
+	t_child_params	params;
 
 	if (exec_invalid_pipe_redirect(list, lst_env, &var))
 		return ;
 	var->last_pid = -1;
 	var->in_fd = STDIN_FILENO;
+	params.lst_env = lst_env;
+	params.env = env;
+	params.var = var;
 	while (list)
 	{
 		exec = gnl_to_array(list->args);
-		if (list->next)
-			pipe(var->fd);
-		else
-			initialize_fds(&var->fd[0], &var->fd[1]);
-		var->pid = fork();
-		if (var->pid == 0)
-			run_in_child(list, var, lst_env, env, &exec);
+		params.fnl = list;
+		setup_pipes_and_fork(list, var, &params, exec);
 		if (var->in_fd != STDIN_FILENO)
 			close(var->in_fd);
 		if (list->next)
